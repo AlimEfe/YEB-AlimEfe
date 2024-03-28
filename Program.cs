849075c23c3de7bdd2b32154d9e22a988a2e22db
AlimEfe <160228104+AlimEfe@users.noreply.github.com>
2024-03-28 17:53:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace hello;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    
+   Console.WriteLine(" bir sayı giriniz");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <10 ; i++)
+{
+    Console.WriteLine(i);
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
